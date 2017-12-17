3e92b6766d7f755cbb2e41b566b6182ff0b0438d
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-12-16 22:32:55 -0500

    Populate secondary table view

--- Contacts/Controllers/ContactDetailController.swift
+++ Contacts/Controllers/ContactDetailController.swift
@@ -9,20 +9,29 @@
 import UIKit
 
 class ContactDetailController: UITableViewController {
-    
     var contact: Contact?
+    
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    @IBOutlet weak var streetAddressLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var zipCodeLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+    func configureView() {
+        guard let contact = contact else { return }
+        
+        phoneNumberLabel.text = contact.phone
+        emailLabel.text = contact.email
+        
+        streetAddressLabel.text = contact.street
+        cityLabel.text = contact.city
+        stateLabel.text = contact.state
+        zipCodeLabel.text = contact.zip
     }
 }
--- Contacts/Controllers/ContactListController.swift
+++ Contacts/Controllers/ContactListController.swift
@@ -25,4 +25,12 @@ class ContactListController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return contacts.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath)
+        
+        let contact = contacts[indexPath.row]
+        
+        return cell
+    }
 }
