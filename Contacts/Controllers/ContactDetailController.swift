4cfad0160324345aea8a6e17a1da5e8ed38d1845
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-12-16 21:54:01 -0500

    Implement data source functions for main table view

---
+++ Contacts/Controllers/ContactDetailController.swift
@@ -0,0 +1,28 @@
+//
+//  ContactDetailController.swift
+//  Contacts
+//
+//  Created by Mohammed Al-Dahleh on 2017-12-16.
+//  Copyright © 2017 Mohammed Al-Dahleh. All rights reserved.
+//
+
+import UIKit
+
+class ContactDetailController: UITableViewController {
+    
+    var contact: Contact?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
---
+++ Contacts/Controllers/ContactListController.swift
@@ -0,0 +1,28 @@
+//
+//  ContactListController.swift
+//  Contacts
+//
+//  Created by Mohammed Al-Dahleh on 2017-12-16.
+//  Copyright © 2017 Mohammed Al-Dahleh. All rights reserved.
+//
+
+import UIKit
+
+class ContactListController: UITableViewController {
+    
+    var contacts = ContactsSource.contacts
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+}
